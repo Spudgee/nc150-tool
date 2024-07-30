1b24f3e6a085032df73723c6c291114643677d4d
Cooper Agustin <cooper.agustin@protonmail.com>
2024-07-31 00:23:21 +1000

    category_initializer: fixed child/parent relationships

--- nc150_tool/category_initializer.py
+++ nc150_tool/category_initializer.py
@@ -4,41 +4,36 @@ from constants import *
 def initialize_categories():
     categories = {}
 
+    # Parental relationships between categories
     arrays_and_hashing = Category(ARRAYS_AND_HASHING)
     two_pointers = Category(TWO_POINTERS, [ARRAYS_AND_HASHING])
     stack = Category(STACK, [ARRAYS_AND_HASHING])
-    binary_search = Category(BINARY_SEARCH, [ARRAYS_AND_HASHING])
-    sliding_window = Category(SLIDING_WINDOW, [ARRAYS_AND_HASHING])
-    linked_list = Category(LINKED_LIST)
-    trees = Category(TREES)
+    binary_search = Category(BINARY_SEARCH, [TWO_POINTERS])
+    sliding_window = Category(SLIDING_WINDOW, [TWO_POINTERS])
+    linked_list = Category(LINKED_LIST, [TWO_POINTERS])
+    trees = Category(TREES, [BINARY_SEARCH, LINKED_LIST])
     tries = Category(TRIES, [TREES])
-    backtracking = Category(BACKTRACKING)
-    heap_priority_queue = Category(HEAP_PRIORITY_QUEUE, [BACKTRACKING])
-    graphs = Category(GRAPHS)
-    advanced_graphs = Category(ADVANCED_GRAPHS, [GRAPHS])
-    one_d_dp = Category(ONE_D_DP)
-    two_d_dp = Category(TWO_D_DP, [ONE_D_DP])
-    greedy = Category(GREEDY)
-    intervals = Category(INTERVALS)
-    math_and_geometry = Category(MATH_AND_GEOMETRY)
+    backtracking = Category(BACKTRACKING, [TREES])
+    heap_priority_queue = Category(HEAP_PRIORITY_QUEUE, [TREES])
+    graphs = Category(GRAPHS, [BACKTRACKING])
+    advanced_graphs = Category(ADVANCED_GRAPHS, [GRAPHS, HEAP_PRIORITY_QUEUE])
+    one_d_dp = Category(ONE_D_DP, [BACKTRACKING])
+    two_d_dp = Category(TWO_D_DP, [ONE_D_DP, GRAPHS])
+    greedy = Category(GREEDY, [HEAP_PRIORITY_QUEUE])
+    intervals = Category(INTERVALS, [HEAP_PRIORITY_QUEUE])
+    math_and_geometry = Category(MATH_AND_GEOMETRY, [GRAPHS, BIT_MANIPULATION])
     bit_manipulation = Category(BIT_MANIPULATION, [TWO_D_DP])
 
-    # Establish parent-child relationships
-    arrays_and_hashing.children = [two_pointers, stack, binary_search, sliding_window]
-    two_pointers.parents = [arrays_and_hashing]
-    stack.parents = [arrays_and_hashing]
-    binary_search.parents = [arrays_and_hashing]
-    sliding_window.parents = [arrays_and_hashing]
-    trees.children = [tries]
-    tries.parents = [trees]
-    backtracking.children = [heap_priority_queue]
-    heap_priority_queue.parents = [backtracking]
-    graphs.children = [advanced_graphs]
-    advanced_graphs.parents = [graphs]
-    one_d_dp.children = [two_d_dp]
-    two_d_dp.parents = [one_d_dp]
-    two_d_dp.children = [bit_manipulation]
-    bit_manipulation.parents = [two_d_dp]
+    # Set children of different categories
+    arrays_and_hashing.children = [two_pointers, stack]
+    two_pointers.children = [binary_search, sliding_window, linked_list]
+    binary_search.children = [trees]
+    linked_list.children = [trees]
+    trees.children = [tries, heap_priority_queue, backtracking]
+    heap_priority_queue.children = [intervals, greedy, advanced_graphs]
+    backtracking.children = [graphs, one_d_dp]
+    graphs.children = [advanced_graphs, math_and_geometry, two_d_dp]
+    one_d_dp.children = [two_d_dp, bit_manipulation]
 
     # Add categories to the dictionary
     categories[ARRAYS_AND_HASHING] = arrays_and_hashing
