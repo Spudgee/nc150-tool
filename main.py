fc448900bee5f1174b0f1294f78dd9d1732edaeb
Cooper Agustin <cooper.agustin@protonmail.com>
2024-07-30 19:01:24 +1000

    main: display options to access download manager or problem tracker

---
+++ main.py
@@ -0,0 +1,8 @@
+# Display main menu options for overall Neetcode manager
+def display_main_menu():
+    print("\nNeetCode 150 Manager")
+    print("1. Download manager")
+    print("2. Problem Tracker")
+    print("3. Exit")
+    return input("Enter your choice: ")
+
